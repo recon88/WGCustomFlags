37eca1d546401450707d1afcdad6b34353f2e276
Gabriele C <sgdc3.mail@gmail.com>
2016-08-06 11:42:26 +0200

    Cleanup

--- src/main/java/com/mewin/WGCustomFlags/util/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) by mewin
- * All rights reserved.
- */
-
-/**
- * utility classes that do not directly refer to the plugin
- */
-package com.mewin.WGCustomFlags.util;
--- src/main/java/com/mewin/util/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) by mewin
- * All rights reserved.
- */
-
-/**
- * utility classes that are created to be used by other plugins
- */
-package com.mewin.util;
